1880fb2256f9d84b4ab1fd5c0fafbd6c95cad9b3
Steve <sfarrugia@gmail.com>
2021-10-28 17:02:11 +0200

    exercise 1_21 to 1_24

---
+++ exercise_1_23.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+/*
+    write a program to remove all comments
+    from a C program. Don't forget to
+    handle quoted strings and character
+    constants properly. C comments do not
+    nest.
+*/
---
+++ exercise_1_24.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*
+    Write a program to check a C program
+    for rudimentary syntax like
+    unbalanced parentheses, brackets
+    and braces. Don't forget about
+    qoutes, both single and double,
+    escape sequences, and comments.
+    (This program is hard if you do
+    it in full generality.)
+*/
