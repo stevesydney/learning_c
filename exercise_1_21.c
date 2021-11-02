e6348650c3cd827c8c0fe4f08f9ba636a2f1a522
Steve Linux <sfarrugia@gmail.com>
2021-11-02 18:35:06 +0100

    21 solution from book but not working right

--- exercise_1_21.c
+++ exercise_1_21.c
@@ -10,3 +10,40 @@
     to reach a tab stop, which should
     be given preference?
 */
+
+#define TABSIZE 8
+
+int main()
+{
+    int c;
+    int bs;
+    int ts;
+    int pos;
+
+    bs = 0;
+    ts = 0;
+
+    for (pos = 1; (c = getchar()) != EOF; ++pos) {
+        if (c == ' ') {
+            if (pos % TABSIZE != 0)
+                ++bs;
+            else {
+                bs = 0;
+                ++ts;
+            }
+        } else {
+            for ( ; ts > 0; --ts)
+                putchar('t');
+            if (c == '\t')
+                bs = 0;
+            else
+                for ( ; bs > 0; --bs)
+                    putchar('s');
+            putchar(c);
+            if (c == '\n')
+                pos = 0;
+            else if (c == '\t')
+                pos = pos + (TABSIZE - (pos - 1) % TABSIZE) - 1;
+        }
+    }
+}
